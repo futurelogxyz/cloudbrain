ed0784ed4ad653c4ffa069c570a31c94351975c8
wallezen <judezhang@tencent.com>
2024-02-19 16:16:01 +0800

    Quartz sync: Feb 19, 2024, 4:16 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🧠 Cloud Brain",
+    pageTitle: "🧠🌿 The Matrix",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
